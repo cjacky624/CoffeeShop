765d08e0cc8bac484ab74bc3864196394ae3ac49
Jacky Chan <cjacky624@gmail.com>
2018-11-08 06:54:34 -0500

    First Commit

---
+++ CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using CoffeeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult UserRegistration()
+        {
+            return View();
+        }
+        public ActionResult AddUser(UserInfo newUser)
+        {
+            {
+
+                if (ModelState.IsValid)
+                {
+
+                    ViewBag.ConfMessage = "Welcome " + newUser.FirstName;
+                    ViewBag.Name = $"Name: {newUser.FirstName} {newUser.LastName}";
+                    ViewBag.Email = $"Email: {newUser.Email}";
+
+                    return View("Summary");
+                }
+
+                else
+                {
+                    return View("Error");
+                }
+            }
+        }
+    }
+}
---
+++ CoffeeShop/Models/UserInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Models
+{
+    public class UserInfo
+    {
+        [Required] 
+        [RegularExpression("^[a-zA-Z]{2,}$")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [RegularExpression("^[a-zA-Z]{2,}$")]
+        public string LastName { get; set; }
+
+
+        public string Email { get; set; }
+
+        public UserInfo()
+        {
+            FirstName = "";
+            LastName = "";
+        }
+        public UserInfo(string firstName, string lastName, string eMail)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = eMail;
+        }
+
+    }
+}
